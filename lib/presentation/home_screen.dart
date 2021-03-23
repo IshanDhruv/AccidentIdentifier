4c690d484ba1ac5134ec1d5d7bd44baf5511fae1
IshanDhruv <ishandhruv22@gmail.com>
2021-03-23 12:45:09 +0530

    Added bottom nav bar

--- lib/presentation/home_screen.dart
+++ lib/presentation/home_screen.dart
@@ -13,6 +13,13 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   Auth auth = Auth();
 
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +31,25 @@ class _HomeScreenState extends State<HomeScreen> {
             onPressed: () => auth.signOut(),
           )
         ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Contacts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.purple[800],
+        onTap: _onItemTapped,
       ),
     );
   }

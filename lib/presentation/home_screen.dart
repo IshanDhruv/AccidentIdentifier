1b51cd0e1a7d04df5ab0761d99c350fc0cf2ad77
IshanDhruv <ishandhruv22@gmail.com>
2021-04-02 12:06:09 +0530

    Added location fetching

--- lib/presentation/home_screen.dart
+++ lib/presentation/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -7,9 +10,134 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
+  void initState() {
+    Geolocator.checkPermission().then((value) => print(value));
+    Geolocator.requestPermission().then((value) => print(value));
+    super.initState();
+  }
+
+  final List<_PositionItem> _positionItems = <_PositionItem>[];
+  StreamSubscription<Position> _positionStreamSubscription;
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Home"),
+    return Scaffold(
+      body: Builder(
+        builder: (context) {
+          if (_positionItems.isNotEmpty) {
+            final positionItem = _positionItems.last;
+            if (positionItem.type == _PositionItemType.permission) {
+              return ListTile(
+                title: Text(positionItem.displayValue,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    )),
+              );
+            } else {
+              return Card(
+                child: ListTile(
+                  title: Text(
+                    positionItem.displayValue,
+                  ),
+                ),
+              );
+            }
+          } else
+            return Container();
+        },
+      ),
+      floatingActionButton: Stack(
+        children: <Widget>[
+          Positioned(
+            bottom: 10.0,
+            right: 10.0,
+            child: FloatingActionButton.extended(
+                onPressed: () async {
+                  await Geolocator.getCurrentPosition().then((value) => {
+                        _positionItems.add(_PositionItem(
+                            _PositionItemType.position, value.toString()))
+                      });
+
+                  setState(
+                    () {},
+                  );
+                },
+                label: Text("Current Position")),
+          ),
+          Positioned(
+            bottom: 80.0,
+            right: 10.0,
+            child: FloatingActionButton.extended(
+              onPressed: _toggleListening,
+              label: Text(() {
+                if (_positionStreamSubscription == null) {
+                  return "Start stream";
+                } else {
+                  final buttonText =
+                      _positionStreamSubscription.isPaused ? "Resume" : "Pause";
+
+                  return "$buttonText stream";
+                }
+              }()),
+              backgroundColor: _determineButtonColor(),
+            ),
+          ),
+        ],
+      ),
     );
   }
+
+  bool _isListening() => !(_positionStreamSubscription == null ||
+      _positionStreamSubscription.isPaused);
+
+  Color _determineButtonColor() {
+    return _isListening() ? Colors.green : Colors.red;
+  }
+
+  void _toggleListening() {
+    if (_positionStreamSubscription == null) {
+      final positionStream = Geolocator.getPositionStream();
+      _positionStreamSubscription = positionStream.handleError((error) {
+        _positionStreamSubscription?.cancel();
+        _positionStreamSubscription = null;
+      }).listen((position) => setState(() => _positionItems.add(
+          _PositionItem(_PositionItemType.position, position.toString()))));
+      _positionStreamSubscription?.pause();
+    }
+
+    setState(() {
+      if (_positionStreamSubscription == null) {
+        return;
+      }
+
+      if (_positionStreamSubscription.isPaused) {
+        _positionStreamSubscription.resume();
+      } else {
+        _positionStreamSubscription.pause();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    if (_positionStreamSubscription != null) {
+      _positionStreamSubscription.cancel();
+      _positionStreamSubscription = null;
+    }
+
+    super.dispose();
+  }
+}
+
+enum _PositionItemType {
+  permission,
+  position,
+}
+
+class _PositionItem {
+  _PositionItem(this.type, this.displayValue);
+
+  final _PositionItemType type;
+  final String displayValue;
 }
